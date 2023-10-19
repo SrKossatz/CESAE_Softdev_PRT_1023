2b9fd23490f66a2daff9926b7f97108db7aff5f7
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-10-19 22:09:40 +0100

    Resolução Ex03

--- ProjetoJava/src/FichaPratica01/Ex03.java
+++ ProjetoJava/src/FichaPratica01/Ex03.java
@@ -1,4 +1,22 @@
 package FichaPratica01;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ex03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int area, base, height, perimeter;
+
+        System.out.print("Insira o valor da base do retângulo: ");
+        base = input.nextInt();
+
+        System.out.print("Insira o valor da altura do retângulo: ");
+        height = input.nextInt();
+
+        area = base * height;
+        perimeter = base * 2 + height * 2;
+        System.out.println("A Área do retângulo é de: " + area + "m, e o Perímetro é de: " + perimeter + "m");
+    }
 }
