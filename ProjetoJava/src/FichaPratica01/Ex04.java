091d6ecef8b28dd656908b4e8ac05c6659b38c7e
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-10-19 22:40:10 +0100

    Resolução Ex04

--- ProjetoJava/src/FichaPratica01/Ex04.java
+++ ProjetoJava/src/FichaPratica01/Ex04.java
@@ -1,4 +1,33 @@
 package FichaPratica01;
 
+
+import java.util.Scanner;
+
+// fórmula de cáculo A = π . r2
+
 public class Ex04 {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    double area,pi, radius;
+
+
+    System.out.print("Insira o raio da circuferência: ");
+    radius = input.nextDouble();
+    radius = radius * radius;
+
+    pi = 3.14;
+    area = pi * radius;
+
+    System.out.println("A área total da circuferência é de: " + area + "cm2");
+
+
+
+
+
+
+
+
+  }
 }
