a6fad3516b66b187d595b3dc64f16fa6cec62353
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-10-21 00:42:37 +0100

    Finalizado F02EX05

--- ProjetoJava/src/FichaPratica02/Ex05.java
+++ ProjetoJava/src/FichaPratica02/Ex05.java
@@ -1,4 +1,23 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Ex05 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int value1, value2;
+    System.out.print("Insira o primeiro valor: ");
+    value1= input.nextInt();
+
+    System.out.print("Insira o segundo valor: ");
+    value2= input.nextInt();
+
+    if (value1 > value2){
+      System.out.print(value2);
+    }else{
+      System.out.println(value1);
+    }
+
+  }
 }
