928c7a312376c4b8d6d3a37edf9bbfd948a8b72b
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-10-22 16:28:51 +0100

    Finalizado F02EX07

--- ProjetoJava/src/FichaPratica02/Ex07.java
+++ ProjetoJava/src/FichaPratica02/Ex07.java
@@ -5,6 +5,16 @@ public class Ex07 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
+    int number;
+
+    System.out.print("Insira um número inteiro e direi se é PAR ou ÍMPAR: ");
+    number = input.nextInt();
+
+    if (number % 2 == 0){
+      System.out.println("Este número é PAR");
+    }else{
+      System.out.println("Este número é ÍMPAR");
+    }
 
   }
 }
