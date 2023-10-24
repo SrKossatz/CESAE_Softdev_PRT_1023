38da61d40e411c0fc8d8b8ca971fa2578aef64ff
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-10-24 22:25:28 +0100

    Finalizado F02EX14

--- ProjetoJava/src/FichaPratica02/Ex14.java
+++ ProjetoJava/src/FichaPratica02/Ex14.java
@@ -7,24 +7,40 @@ public class Ex14 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    int number1, number2, number3;
+    int a, b, c;
 
     System.out.print("Insira o primeiro número: ");
-    number1 = input.nextInt();
+    a = input.nextInt();
 
     System.out.print("Insira o segundo número: ");
-    number2 = input.nextInt();
+    b = input.nextInt();
 
     System.out.print("Insira o terceiro número: ");
-    number3 = input.nextInt();
+    c = input.nextInt();
 
-    if (number1<number2 && number1<number3){
-      if (number2<number3){
-        System.out.println(number1+" "+number2+ " "+number3);
+    if (a<b && a<c){ //a
+      if (b<c){
+        System.out.println(a+" "+b+" "+c);
+      }else {
+        System.out.println(a+" "+c+" "+b);
       }
     }
 
+    if (b<c && b<a){ //b
+      if (a<c){
+        System.out.println(b+" "+a+" "+c);
+      }else{
+        System.out.println(b+" "+c+" "+a);
+      }
+    }
 
+    if (c<b && c<a){ //c
+      if (a<b){
+        System.out.println(c+" "+a+" "+b);
+      }else{
+        System.out.println(c+" "+b+" "+a);
+      }
+    }
 
   }
 }
