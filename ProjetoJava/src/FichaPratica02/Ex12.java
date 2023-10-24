681edf0cf8fcdeeba41f1fb032ead3a91ed3ab5a
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-10-24 10:37:38 +0100

    Finalizado F02EX12

---
+++ ProjetoJava/src/FichaPratica02/Ex12.java
@@ -0,0 +1,39 @@
+package FichaPratica02;
+
+import java.util.Scanner;
+
+public class Ex12 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int selectedOption;
+
+    System.out.println("Bem vindo ao menu interativo, digite a opção desejada: ");
+    System.out.println("Opção 1: Criar.");
+    System.out.println("Opção 2: Atualizar.");
+    System.out.println("Opção 3: Eliminar.");
+    System.out.println("Opção 4: Sair.");
+    
+    selectedOption= input.nextInt();
+
+    switch (selectedOption){
+      case 1:
+        System.out.println("Opção 1 selecionada.");
+        break;
+      case 2:
+        System.out.println("Opção 2 selecionada.");
+        break;
+      case 3:
+        System.out.println("Opção 3 selecionada.");
+        break;
+      case 4:
+        System.out.println("");
+        break;
+      default:
+        System.out.println("Opção inválida");
+
+    }
+
+
+  }
+}
