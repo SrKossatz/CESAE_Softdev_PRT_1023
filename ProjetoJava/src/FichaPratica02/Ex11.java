903a6796d114db2598711941f16dc0f098ac00ff
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-10-24 10:16:01 +0100

    Finalizado F02EX11

--- ProjetoJava/src/FichaPratica02/Ex11.java
+++ ProjetoJava/src/FichaPratica02/Ex11.java
@@ -6,6 +6,21 @@ public class Ex11 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
+    double finalBalance,balance,amount;
+
+    System.out.print("Digite o saldo da conta bancária: ");
+    balance = input.nextDouble();
+
+    System.out.print("Digite o montante a creditar/debitar: ");
+    amount = input.nextDouble();
+
+    finalBalance = balance + amount;
+
+    if (finalBalance >= 0) {
+      System.out.println("Operação realizada com sucesso. Saldo final: " + finalBalance);
+    } else {
+      System.out.println("A operação não é válida. Você não tem saldo suficiente.");
+    }
 
 
   }
