2c5e36e051d2ebb5f48ebb7ed6d004e7b23347b3
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-11-11 11:14:27 +0000

    F05Ex01 ATT

--- ProjetoJava/src/FichaPratica05/Ex01.java
+++ ProjetoJava/src/FichaPratica05/Ex01.java
@@ -3,28 +3,20 @@ package FichaPratica05;
 import java.util.Scanner;
 
 public class Ex01 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-        int[] vetor = new int[10];
+    int[] vetor = new int[10];
 
-        for (int indice=0; indice <=9; indice++){
+    for (int i = 0; i < vetor.length; i++) {
+      System.out.print("Insira o seu número no Vetor ["+i+"]: ");
+      vetor[i] = input.nextInt();
+    }
 
-            System.out.print("Insira o número do array ["+indice+"]: ");
-            vetor [indice] = input.nextInt();
-
-        }
-
-        for (int indice=0; indice<=9; indice++){
-            System.out.println("vetor ["+indice+"]: " + vetor[indice]);
-        }
-
-
-
-
-
-
-
+    for (int i = 0; i < vetor.length; i++) {
+      System.out.println("Vetor["+i+"]: "+vetor[i]);
 
     }
+
+  }
 }
