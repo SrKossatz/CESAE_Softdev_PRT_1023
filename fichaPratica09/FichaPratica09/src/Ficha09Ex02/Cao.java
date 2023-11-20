a25fb807ef66ad329f89f72910e858822bb568fa
ATYLLA KOSSATZ <atyllakossatz@gmail.com>
2023-11-20 10:29:31 +0000

    Ficha09Ex02

--- fichaPratica09/FichaPratica09/src/Ficha09Ex02/Cao.java
+++ fichaPratica09/FichaPratica09/src/Ficha09Ex02/Cao.java
@@ -2,32 +2,32 @@ package Ficha09Ex02;
 
 public class Cao {
 
+    //Classe defini um conjunto de atributos
+    // Bloco dos construtores C/lista de parâmetros
+    // lista de Métodos
+    // get para ver
+    //sett para alterar
+
+
     private String nome;
     private String raca;
 
     private String ladrar="Au au au";
 
 
-    public Cao(String nome, String raca) {
+    public Cao(String nome, String raca) { // Método construtor é público para que seja acedido dentro da própria classe.
         this.nome = nome;
         this.raca = raca;
+
     }
 
-   public void ladrar(){
-       System.out.println("ola");
 
-   }
-
-    public String getNome() {
-        return nome;
+    public void setLadrar(String ladrar) {
+        this.ladrar = ladrar;
     }
 
-    public String getRaca() {
-        return raca;
-    }
-
-    public String getLatido() {
-        return ladrar;
+    public void ladrar(){
+        System.out.println(this.ladrar);
     }
 
 
--- fichaPratica09/FichaPratica09/src/Ficha09Ex02/Main.java
+++ fichaPratica09/FichaPratica09/src/Ficha09Ex02/Main.java
@@ -2,10 +2,28 @@ package Ficha09Ex02;
 
 public class Main {
 
-    Cao cao1 = new Cao("lucky", "Yourk Shire");
-    Cao cao2 = new Cao("Golias", "Pastor Alemão");
-    Cao cao3 = new Cao("Thor", "Vira Lata");
+
+
+    public static void main(String[] args) {
+
+        // No main precisa invocar o método construtor
+        Cao cao1 = new Cao("lucky", "Yourk Shire");
+        Cao cao2 = new Cao("Golias", "Pastor Alemão");
+        Cao cao3 = new Cao("Thor", "Vira Lata");
 
 
 
+        cao1.ladrar();
+        cao2.ladrar();
+        cao3.ladrar();
+
+        System.out.println();
+
+        // Alterar o latido do cao1
+        cao1.setLadrar("wooof!");
+
+        cao1.ladrar();
+        cao2.ladrar();
+        cao3.ladrar();
+    }
 }
